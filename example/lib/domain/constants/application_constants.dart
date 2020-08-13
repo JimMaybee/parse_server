7c1bac75994c70a22d2e3432f6b458046a07b531
JimMaybee <jmaybee@eBusiness-Solutions.ca>
2020-08-13 14:01:09 -0400

    First Commit

---
+++ example/lib/domain/constants/application_constants.dart
@@ -0,0 +1,5 @@
+const String keyApplicationName = 'EBS-Inspector';
+const String keyParseApplicationId = 'a3PjngX9q2BLbHsjCsfRtCb9GLewJtu15bSTFZEu';
+const String keyParseMasterKey = 'jXjeUhumMHa5nqHsVUYVIdanCbFuDkTeDDGrkzpP';
+const String keyParseServerUrl = 'https://parseapi.back4app.com/';
+const bool keyDebug = true;
